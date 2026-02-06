713819dad8c5d4b1c2535b2ebc594e72060e7a9a
tjsnpark-stack <tjsn.park@gmail.com>
2026-02-06 17:58:43 +0900

    260206 삼성꿈장학재단

---
+++ sdream/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        autoplay: {  /* 팝업 자동 실행 */
+            delay: 1000,
+            disableOnInteraction: true,
+        },
+        effect: "fade", /* fade 효과 */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+    });//visual_swiper
+})//$(document).ready
