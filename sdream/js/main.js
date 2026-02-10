ace90952903f36a12e8ce80a390fd88b67740e30
tjsnpark-stack <tjsn.park@gmail.com>
2026-02-10 18:04:43 +0900

    260210 삼성꿈

--- sdream/js/main.js
+++ sdream/js/main.js
@@ -43,4 +43,30 @@ $(document).ready(function(){
         header_fixed()
     })
 
+    /************************************************************
+     웹진 swiper
+     **********************************************************/
+     const webzine_swiper = new Swiper('.webzine .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            // 541: {    /* 768-540 사이 */
+            //     slidesPerView: 2,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+            //     spaceBetween: 16,
+            // },
+            769: {    /* 769px 이상일때 적용 */
+                slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: false,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        
+        navigation: {
+            nextEl: '.webzine .ctrl_wrap .next',
+            prevEl: '.webzine .ctrl_wrap .prev',
+        },
+    });
+
+
 })//$(document).ready
