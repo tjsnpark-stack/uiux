6b3cecf406d4248d48216308b7807d33537cbf6f
tjsnpark-stack <tjsn.park@gmail.com>
2026-03-13 16:56:11 +0900

    260313 생명의숲

--- forest/js/main.js
+++ forest/js/main.js
@@ -14,28 +14,30 @@ $(document).ready(function(){
 
 		verticalCentered: true, /* 컨텐츠 요소 위아래 가운데 */
 		
-		scrollOverflow: true, /* 컨텐츠가 넘쳐도 스크롤 금지 */
+		scrollOverflow: false, /* 컨텐츠가 넘쳐도 스크롤 금지 */
 
 		afterLoad: function(origin, destination, direction, trigger){
 			if(destination.index == 0){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
-				// console.log('1번째 슬라이드가 로딩 되었을때');
+				//console.log('1번째 슬라이드가 로딩 되었을때');
 				$('.sec_nav').attr('data-bg', '')
 				$('.header').attr('data-bg', '')
 			}else if(destination.index == 1){
-				// console.log('2번째 슬라이드가 로딩 되었을때');
+				//console.log('2번째 슬라이드가 로딩 되었을때');
 				$('.sec_nav').attr('data-bg', 'white')
 				$('.header').attr('data-bg', 'white')
 				$('.tree .counter').counterUp();
 			}else if(destination.index == 2){
-				// console.log('3번째 슬라이드가 로딩 되었을때');
+				//console.log('3번째 슬라이드가 로딩 되었을때');
+				$('.sec_nav').attr('data-bg', 'blue')
+				$('.header').attr('data-bg', 'mo')
 			}else if(destination.index == 3){
+				//console.log('4번째 슬라이드가 로딩 되었을때');
 				$('.sec_nav').attr('data-bg', 'white')
 				$('.header').attr('data-bg', 'white')
-				// console.log('4번째 슬라이드가 로딩 되었을때');
 			}else if(destination.index == 4){
+				//console.log('5번째 슬라이드가 로딩 되었을때');
 				$('.sec_nav').attr('data-bg', 'none')
 				$('.header').attr('data-bg', 'none')
-				// console.log('5번째 슬라이드가 로딩 되었을때');
 			}
 		},
 
@@ -56,16 +58,37 @@ $(document).ready(function(){
 	})
 
 	/* visual의 swiper 시작 */
-	const swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
-
+	const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
 		autoplay: {  /* 팝업 자동 실행 */
 			delay: 2500,
 			disableOnInteraction: true,
 		},
-
 		effect: "fade", /* fade 효과 */
 		loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
 	});
-	/* visual의 swiper 종료 */
+	/* visual의 swiper 끝 */
+
+	
+	const story_swiper = new Swiper('.story .swiper', { /* 팝업을 감싼는 요소의 class명 */
+		slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+		spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+		breakpoints: {
+			450: {    /* 640px 이상일때 적용 */
+				slidesPerView: 2,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+				spaceBetween: 16,
+			},
+			768: {    /* 640px 이상일때 적용 */
+				slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+				spaceBetween: 24,
+			},
+			1280: {    /* 640px 이상일때 적용 */
+				slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+				spaceBetween: 24,
+			},
+		},
+		//centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+		loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+	});
+	
 
 })//$(document).ready
