d53d543b7a2c79f6974c2702fd5d1b5c444f841e
tjsnpark-stack <tjsn.park@gmail.com>
2026-03-20 18:03:01 +0900

    260320 아이조아

--- ijoa/js/main.js
+++ ijoa/js/main.js
@@ -43,6 +43,20 @@ $(document).ready(function(){
         header_fixed()
     })
 
+    const service_swiper = new Swiper('.service  .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        autoplay: {  /* 팝업 자동 실행 */
+            delay: 5000,
+            disableOnInteraction: true,
+        },
+        //effect: "fade", /* fade 효과 */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        navigation: {
+            nextEl: '.service .next',
+            prevEl: '.service .prev',
+        }
+    });//service_swiper
+
+
     /************************************************************
      웹진 swiper
      **********************************************************/
