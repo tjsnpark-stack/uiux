bb13a1111c95550aebd892812544bf92ffd82ace
tjsnpark-stack <tjsn.park@gmail.com>
2026-04-10 18:01:18 +0900

    260410 미러리스월드

---
+++ mirror/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+var swiper = new Swiper(".c_type .swiper", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+    });
+
+    
+    
+    
+});
