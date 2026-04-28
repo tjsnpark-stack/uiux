26d8a89ffc11e31dbe1c5404bbc3ef933f171c47
tjsnpark-stack <tjsn.park@gmail.com>
2026-04-28 17:59:15 +0900

    260428 wwf_new

--- wwf_new/js/main_new.js
+++ wwf_new/js/main_new.js
@@ -1,4 +1,49 @@
 $(document).ready(function(){
+
+    let device_status
+    let window_w
+    let mobile_size = 1024
+    function device_chk(){
+        window_w = $(window).width()
+        if(window_w > mobile_size){
+            device_status = 'pc'
+        }else{
+            device_status = 'mo'
+        }
+        console.log(device_status)
+    }
+    device_chk()
+    $(window).resize(function(){
+        device_chk()
+    })
+    $('.header .gnb .gnb_wrap ul.depth1 > li').on('mouseenter', function(){
+        $('.header').addClass('menu_over')
+        $('.header .gnb .gnb_wrap ul.depth1 > li').removeClass('over')
+        $(this).addClass('over')
+    })
+    $('.header .gnb .gnb_wrap').on('mouseleave', function(){
+        $('.header').removeClass('menu_over')
+        $('.header .gnb .gnb_wrap ul.depth1 > li').removeClass('over')
+    })
+
+
+    $('.header .gnb .gnb_wrap ul.depth1 > li > a').on('click', function(e){
+		e.preventDefault();		/* a 태그의 href를 작동 시키지 않음 */
+        if($(this).parent().hasClass('open') == true){
+            $(this).parent().removeClass('open')
+        }else{  
+            $('.header .gnb .gnb_wrap ul.depth1 > li').removeClass('open')
+            $(this).parent().addClass('open')
+        }
+	});
+    $('.header .gnb .gnb_open').on('click', function(){
+        $('.header').addClass('menu_open')
+    })
+    $('.header .gnb .gnb_wrap .gnb_close').on('click', function(){
+        $('.header').removeClass('menu_open')
+    })
+
+
     let visual_wrap = $('.visual')
     let visual_wrap_h
     let visual_item = $('.visual .area')
@@ -11,10 +56,6 @@ $(document).ready(function(){
     let visual_step //현재 몇번째 이미지가 슬라이드 되고 있는지
     let visual_percent
 
-    /************************* 모바일 대응용 **********************/
-    visual_wrap.height(win_h * 3);
-    $('.visual .visual_wrapper').height(win_h)
-    $('.visual .area .inner').innerHeight(win_h)
 
     function visual_scroll(){
         scrolling = $(window).scrollTop()
@@ -28,15 +69,18 @@ $(document).ready(function(){
 
         if(scrolling < visual_start){
             // 시작 전
+            visual_wrap.attr('data-staus', 'before')
             visual_item.height(0)
             visual_item.eq(0).height('100%')
 
         }else if(scrolling > visual_end){
             // 끝난 후
+            visual_wrap.attr('data-staus', 'after')
             visual_item.height('100%')
 
         }else{
             // 진행 중
+            visual_wrap.attr('data-staus', 'ing')
             let current = scrolling - visual_start
 
             // 현재 스텝 (0부터 시작)
@@ -63,10 +107,6 @@ $(document).ready(function(){
     })
     $(window).resize(function(){
         visual_scroll()
-        /************************* 모바일 대응용 **********************/
-        visual_wrap.height(win_h * 3);
-        $('.visual .visual_wrapper').height(win_h)
-        $('.visual .area .inner').innerHeight(win_h)
     })
 
     
@@ -207,19 +247,32 @@ $(document).ready(function(){
         spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
         breakpoints: {
 
-            376: {    /* 640px 이상일때 적용 */
+            499: {    /* 640px 이상일때 적용 */
                 slidesPerView: 2,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
                 spaceBetween: 16,
             },
-            880: {    /* 640px 이상일때 적용 */
+            768: {    /* 640px 이상일때 적용 */
                 slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
                 spaceBetween: 16,
             },
-            1025: {    /* 640px 이상일때 적용 */
+            1100: {    /* 640px 이상일때 적용 */
                 slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
                 spaceBetween: 16,
             },
         },
     });
+
+    $('.newsletter .subscript .regist .email .email_domain .domain_open').on('click', function(){
+        console.log('체크')
+        if($('.newsletter .subscript .regist .email .email_domain').hasClass('open') == false){
+            $('.newsletter .subscript .regist .email .email_domain').addClass('open')
+        }else{
+            $('.newsletter .subscript .regist .email .email_domain').removeClass('open')
+        }
+    })
+    $('.newsletter .subscript .regist .email .email_domain .domain_wrap ul li').on('click', function(){
+        $('.newsletter .subscript .regist .email .email_domain .domain_open').text($(this).attr('data-value'))
+        $('.newsletter .subscript .regist .email .email_domain').removeClass('open')
+    })
 
 })//document.ready(맨끝)
