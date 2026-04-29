3ef123a7627b017d1b15c5880ed74821ff2a78bb
tjsnpark-stack <tjsn.park@gmail.com>
2026-04-29 13:37:07 +0900

    260429 wwf_new

--- wwf_new/js/main_new.js
+++ wwf_new/js/main_new.js
@@ -166,75 +166,75 @@ $(document).ready(function(){
     let rgb_obj3 = $('.goal_pc .txt .now a')
 
     function scale_img() {
-        supprt_scrolling = $(window).scrollTop()
-        win_w = $(window).width()
-        window_h = $(window).height()
-        ani_start = obj_area.offset().top
-        ani_end = ani_start + obj_area.innerHeight() - window_h
-        obj_start_x = obj_wrap.offset().left
-        obj_start_y = obj_wrap.offset().top
-        obj_start_w = obj_wrap.width()
-        obj_start_h = obj_wrap.height()
-        //console.log(obj_start_w, obj_start_h)
-        //console.log(obj_area.height(), ani_start, ani_end , supprt_scrolling)
-        if(supprt_scrolling < ani_start){
-            //console.log('시작전')
-            obj_area.removeClass(end_class)
-            obj_name.removeAttr('style')
-        }else if(supprt_scrolling > ani_end){
-            //console.log('종료')
-            obj_area.addClass(end_class)
-            obj_x = -obj_start_x
-            obj_y = ani_end - obj_start_y
-            obj_name.css({
-                position: 'absolute',
-                left: 0,
-                top: 0,
-                transform: `translate(${obj_x}px, ${obj_y}px)`,
-                width: win_w + 'px',
-                height: window_h + 'px',
-                borderRadius : 0
-            })
-        }else{
-            if(supprt_scrolling < ani_start + (obj_area.innerHeight() / ani_percent) - window_h){
-                //console.log('진행중')
-                ani_ratio = (supprt_scrolling - ani_start) / ((ani_start + (obj_area.innerHeight() / ani_percent) - window_h) - ani_start)
-                ani_ratio = Math.max(0, Math.min(1, ani_ratio))
-                //console.log(ani_ratio)
-                obj_w = obj_start_w + (win_w - obj_start_w) * ani_ratio
-                obj_h = obj_start_h + (window_h - obj_start_h) * ani_ratio
-                obj_x = - obj_start_x * ani_ratio
-                obj_y = - obj_start_y * ani_ratio + (supprt_scrolling - ani_start*(1-ani_ratio))
+            supprt_scrolling = $(window).scrollTop()
+            win_w = $(window).width()
+            window_h = $(window).height()
+            ani_start = obj_area.offset().top
+            ani_end = ani_start + obj_area.innerHeight() - window_h
+            obj_start_x = obj_wrap.offset().left
+            obj_start_y = obj_wrap.offset().top
+            obj_start_w = obj_wrap.width()
+            obj_start_h = obj_wrap.height()
+            //console.log(obj_start_w, obj_start_h)
+            //console.log(obj_area.height(), ani_start, ani_end , supprt_scrolling)
+            if(supprt_scrolling < ani_start){
+                //console.log('시작전')
+                obj_area.removeClass(end_class)
+                obj_name.removeAttr('style')
+            }else if(supprt_scrolling > ani_end){
+                //console.log('종료')
+                obj_area.addClass(end_class)
+                obj_x = -obj_start_x
+                obj_y = ani_end - obj_start_y
                 obj_name.css({
                     position: 'absolute',
                     left: 0,
                     top: 0,
                     transform: `translate(${obj_x}px, ${obj_y}px)`,
-                    width: obj_w + 'px',
-                    height: obj_h + 'px',
-                    borderRadius : brd_radius - (brd_radius * ani_ratio)
-                })
-                rgb_now = rgb_start + (rgb_end - rgb_start) * ani_ratio
-                rgb_now2 = rgb_start2 + (rgb_end - rgb_start2) * ani_ratio
-                rgb_obj.css('color', 'rgb('+ rgb_now +', '+ rgb_now +', '+ rgb_now +')')
-                rgb_obj2.css('color', 'rgb('+ rgb_now2 +', '+ rgb_now2 +', '+ rgb_now2 +')')
-                rgb_obj3.css('border-color', 'rgb('+ rgb_now2 +', '+ rgb_now2 +', '+ rgb_now2 +')')
-                obj_area.removeClass(end_class)
-            }else{
-                //console.log('고정')
-                obj_area.addClass(end_class)
-                obj_name.css({
-                    position: 'fixed',
-                    left: 0,
-                    top: 0,
-                    transform: 'translate(0, 0)',
                     width: win_w + 'px',
                     height: window_h + 'px',
                     borderRadius : 0
                 })
-            }
+            }else{
+                if(supprt_scrolling < ani_start + (obj_area.innerHeight() / ani_percent) - window_h){
+                    //console.log('진행중')
+                    ani_ratio = (supprt_scrolling - ani_start) / ((ani_start + (obj_area.innerHeight() / ani_percent) - window_h) - ani_start)
+                    ani_ratio = Math.max(0, Math.min(1, ani_ratio))
+                    //console.log(ani_ratio)
+                    obj_w = obj_start_w + (win_w - obj_start_w) * ani_ratio
+                    obj_h = obj_start_h + (window_h - obj_start_h) * ani_ratio
+                    obj_x = - obj_start_x * ani_ratio
+                    obj_y = - obj_start_y * ani_ratio + (supprt_scrolling - ani_start*(1-ani_ratio))
+                    obj_name.css({
+                        position: 'absolute',
+                        left: 0,
+                        top: 0,
+                        transform: `translate(${obj_x}px, ${obj_y}px)`,
+                        width: obj_w + 'px',
+                        height: obj_h + 'px',
+                        borderRadius : brd_radius - (brd_radius * ani_ratio)
+                    })
+                    rgb_now = rgb_start + (rgb_end - rgb_start) * ani_ratio
+                    rgb_now2 = rgb_start2 + (rgb_end - rgb_start2) * ani_ratio
+                    rgb_obj.css('color', 'rgb('+ rgb_now +', '+ rgb_now +', '+ rgb_now +')')
+                    rgb_obj2.css('color', 'rgb('+ rgb_now2 +', '+ rgb_now2 +', '+ rgb_now2 +')')
+                    rgb_obj3.css('border-color', 'rgb('+ rgb_now2 +', '+ rgb_now2 +', '+ rgb_now2 +')')
+                    obj_area.removeClass(end_class)
+                }else{
+                    //console.log('고정')
+                    obj_area.addClass(end_class)
+                    obj_name.css({
+                        position: 'fixed',
+                        left: 0,
+                        top: 0,
+                        transform: 'translate(0, 0)',
+                        width: win_w + 'px',
+                        height: window_h + 'px',
+                        borderRadius : 0
+                    })
+                }
 
-        }
+            }
     }
     scale_img()
     $(window).scroll(function(){
